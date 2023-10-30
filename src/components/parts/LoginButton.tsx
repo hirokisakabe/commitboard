3b54eb6a3cdc7103832de8dd8921eaf82bfdf718
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-10-30 23:22:03 +0900

    ルートページのレイアウトを改善

--- src/components/parts/LoginButton.tsx
+++ src/components/parts/LoginButton.tsx
@@ -4,5 +4,9 @@ import { signIn } from "next-auth/react";
 import { Button } from "@tremor/react";
 
 export function LoginButton() {
-  return <Button onClick={() => signIn()}>Sign in with GitHub</Button>;
+  return (
+    <Button variant="secondary" size="xl" onClick={() => signIn()}>
+      Sign in with GitHub
+    </Button>
+  );
 }
